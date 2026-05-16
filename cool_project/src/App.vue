1528b942c0a5c1cb53174266828b261b99c69c8b
Zainab Gusenova <ZainabGusenova2009@gmail.com>
2026-05-16 12:58:57 +0300

    first commit

---
+++ cool_project/src/App.vue
@@ -0,0 +1,26 @@
+<script>
+  import GameCard from './components/cardGame.vue'
+
+  export default{
+    components: {
+      GameCard
+    }
+  }
+</script>
+
+
+<template>
+  <div>
+    <h1>🎮 Магазин игр</h1>
+      <game-card game="Cyberpunk 2077" :price="1999" />
+      <game-card game="The Witcher 3" :price="1299" />
+      <game-card game="Hollow Knight" :price="399" />
+  </div>
+</template>
+
+<style>
+body {
+  font-family: Arial, sans-serif;
+  padding: 20px;
+}
+</style>
---
+++ cool_project/src/components/cardGame.vue
@@ -0,0 +1,36 @@
+<script>
+    export default {
+        props: {
+            game: String, //название игры
+            price: Number, //цена
+            img: String //картинка
+        },
+    
+
+        methods: {
+            buyGame() {
+                alert(`Оплата совершена. Получена игра ${this.game} за ${this.price}₽`)
+            }
+        },
+    };
+
+</script> 
+
+<template>
+    <div class="card">
+        <h3>{{game}}</h3>
+        <img :src="img">
+        <p>{{price}}₽</p>
+        <button @click="buyGame">Купить</button>
+    </div>
+
+</template>
+
+<style scoped>
+.card {
+  border: 1px solid #ddd;
+  padding: 15px;
+  border-radius: 8px;
+  margin: 10px;
+}
+</style>
